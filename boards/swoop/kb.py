05385d5dd1e1b1aa64a8ab21400a9022ffa4c6b1
Syed Hussaini <masiullah@gmail.com>
2022-04-11 16:47:03 -0500

    ut for capsword and cgswap with improvements

--- boards/swoop/kb.py
+++ boards/swoop/kb.py
@@ -1,10 +1,11 @@
 import board
 
+from kmk.extensions.lock_status import LockStatus
+from kmk.keys import KC
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
 from kmk.matrix import DiodeOrientation
 from kmk.matrix import intify_coordinate as ic
-from kmk.keys import KC
-from kmk.extensions.lock_status import LockStatus
+
 
 class KMKKeyboard(_KMKKeyboard):
 
@@ -23,15 +24,17 @@ class KMKKeyboard(_KMKKeyboard):
     )
     diode_orientation = DiodeOrientation.COLUMNS
     data_pin = board.D13
-    data_pin2= board.D12
+    data_pin2 = board.D12
     encoder_pin_0 = board.D9
     encoder_pin_1 = board.D8
     i2c = board.I2C
 
+    # flake8: noqa
+    # fmt: off
     coord_mapping = [
      0,  1,  2,  3,  4,  20, 21, 22, 23, 24,
      5,  6,  7,  8,  9,  25, 26, 27, 28, 29,
     10, 11, 12, 13, 14,  30, 31, 32, 33, 34,
             17, 18, 19,  35, 36, 37,
     ]
-
+    # fmt: on
